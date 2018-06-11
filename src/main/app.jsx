e4409d5671f7307d688a15da812e07d3ca2dfe78
matheus-lima92 <lima.eca@gmail.com>
2018-06-11 03:09:16 -0300

    included first animation

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -5,10 +5,12 @@ import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import '../css/custom.css';
+import Animation1 from '../animations/animation1';
 
 const props = () => (
   <div className="container">
     <h1>Projeto React</h1>
+    <Animation1 />
   </div>
 );
 
